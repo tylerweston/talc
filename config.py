0e20eb7faf9244830efe3d7b1399e637d470af9d
Tyler Weston <tylerjohnweston@gmail.com>
2022-01-16 22:15:56 -0500

    move soundtrack files

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
+import random
+# TODO: Get this outta here!
+spinner_choice = random.choice(['aesthetic', 'arc', 'arrow3', 'betaWave', 'balloon',
+'bounce', 'bouncingBar', 'circle', 'dots', 'line', 'squish', 'toggle10', 'pong'])
+
+# TODO: Replace these removed soundtracks
+soundtrack_files = [
+    "talc_soundtrack.mp3",
+    "talc_soundtrack2.mp3",
+    "talc_soundtrack3.mp3",
+    "talc_soundtrack4.mp3",
+    # "talc_soundtrack5.mp3",
+]
+
+prog = "TALC"
+author = "Tyler Weston"
+version = '0.0.5'
+
 size = 1280, 720
 
 USE_PROMPTS = False
