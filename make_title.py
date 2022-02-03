c035e50b82b41a3381096a6de222b4032bd96295
Tyler Weston <tylerjohnweston@gmail.com>
2022-02-02 20:13:43 -0500

    add title video generator

---
+++ make_title.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import numpy as np
+from moviepy.editor import *
+import random
+
+last_frame_xor = np.empty([0])
+def xor_frameglitch(clip):
+    
+    def fl(gf, t):
+        global last_frame_xor
+        frame = gf(t)
+        if last_frame_xor.any():
+            lfx = last_frame_xor.copy()
+            np.random.shuffle(lfx)
+            frame = frame * lfx
+        last_frame_xor = frame
+        return frame
+    return clip.fl(fl)
+
+def shuffle_img(clip):
+    def fl(gf, t):
+        frame = gf(t)
+        if random.random() < 0.9:
+            return frame
+        nf = frame.copy()
+        chunk = random.randint(80000, 160000)
+        while True:
+            try:
+                nf = nf.reshape(-1,chunk)
+                break
+            except:
+                chunk = random.randint(80000, 160000)
+        np.random.shuffle(nf)
+        nf = nf.reshape(frame.shape)
+        return nf
+    return clip.fl(fl)
+
+video_title = TextClip(
+    "the aleatoric\nlearning channel", 
+    font='Amiri-Bold', 
+    # fontsize=70, 
+    color='white',
+    stroke_color='black',
+    stroke_width=2, 
+    method='label', 
+    align='Center', 
+    size=(1280, 720)
+)
+video_title_glitched = TextClip(
+    "the aleatoric\nlearning channel", 
+    font='Amiri-Bold', 
+    # fontsize=70, 
+    color='white',
+    stroke_color='black',
+    stroke_width=2, 
+    method='label', 
+    align='Center', 
+    size=(1280, 720)
+)
+
+video_title.duration = 0.5
+video_title_glitched = video_title_glitched.fx(xor_frameglitch).fx(shuffle_img) #.fx(vfx.fadein, 2.5)
+video_title_glitched.duration = 1.5
+title_comped = concatenate_videoclips([video_title, video_title_glitched, video_title], method="compose")
+title_comped.write_videofile("title.mp4", fps=30, codec='libx264', audio=False)
+
