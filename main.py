a0fec954454a588f9dc7ec382112ca06fb064569
Tyler Weston <tylerjohnweston@gmail.com>
2022-08-15 18:59:09 -0400

    work on integrating new yt lib

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ videos gathered from YouTube, and an article grabbed from Wikipedia.
 """
 
 # TODO:
+#   - Fix ffmpeg errors, right now composing videos seems to randomly break sometimes with message
+#     Can't get next frame, or read 0 bytes/##### something like that. Looks like we are reading past
+#     the end of the file?
+#   
+#   - Skip age locked videos in YouTube
 #   - More sound options? Add some random sounds
 #   - Move audio stuff out of summarize
 #   - Better article selection somehow?
@@ -73,7 +78,9 @@ def make_video(use_article=None, args=None):
     keywords, summary, summary_hash_text = summarize_article(wiki_page_content)
 
     # Video clips
+    console.print("[bold green]Getting video clips...")
     random_video_clips = get_random_clips(keywords, wiki_page_title)
+    console.print("[bold green]Done![/bold green]")
 
     # Images
     images_list = get_images(keywords, wiki_page_title, passed_args=args)
@@ -112,6 +119,19 @@ def make_video(use_article=None, args=None):
     generate_and_write_summary(movie_title, summary, keywords)
     console.print(f"Finished writing [bold green]{movie_title}[/bold green]")
 
+            # console.print("Doing final glitching")
+    
+    unified_glitch_pass(f"finished/{movie_title}.mp4", f"finished/{movie_title}_glitched.mp4")
+    # for i in range(5):
+    #     console.print(f"[bold green]Attempting final glitching {i}/5...")
+    #     try:
+    #         unified_glitch_pass(f"finished/{movie_title}.mp4", f"finished/{movie_title}_glitched.mp4")
+    #         break
+    #     except:
+    #         continue
+    # else:
+    #     console.print(f"[bold red]Error[/bold red]: Unable to glitch video")
+
     if not args.no_cleanup:
         console.print("Cleaning up images and audio...")
         cleanup()
@@ -120,6 +140,9 @@ def make_video(use_article=None, args=None):
     console.rule()
 
 def cleanup():
+
+    reset_movie_data()
+
     try:
         shutil.rmtree("downloads")
     except Exception as e:
@@ -140,6 +163,11 @@ def cleanup():
     except Exception as e:
         console.print("[bold red]Warning[/bold red]: Cannot remove narration.mp3")
         console.print(str(e))
+    
+    try:
+        os.remove("title.mp4")
+    except Exception as e:
+        console.print("[bold red]Warning[/bold red]: Cannot remove title.mp4")
 
 
 def open_ai_jank_summary(summary):
@@ -318,29 +346,53 @@ def main():
         make_video(use_article=args.article, args=args)
 
 def display_banner():
-    console.print(" ███████████   █████████   █████         █████████     █████   █████  ███      █████                  ")
-    console.print("░█░░░███░░░█  ███░░░░░███ ░░███         ███░░░░░███   ░░███   ░░███  ░░░      ░░███                   ")
-    console.print("░   ░███  ░  ░███    ░███  ░███        ███     ░░░     ░███    ░███  ████   ███████   ██████   ██████ ")
-    console.print("    ░███     ░███████████  ░███       ░███             ░███    ░███ ░░███  ███░░███  ███░░███ ███░░███")
-    console.print("    ░███     ░███░░░░░███  ░███       ░███             ░░███   ███   ░███ ░███ ░███ ░███████ ░███ ░███")
-    console.print("    ░███     ░███    ░███  ░███      █░░███     ███     ░░░█████░    ░███ ░███ ░███ ░███░░░  ░███ ░███")
-    console.print("    █████    █████   █████ ███████████ ░░█████████        ░░███      █████░░████████░░██████ ░░██████ ")
-    console.print("   ░░░░░    ░░░░░   ░░░░░ ░░░░░░░░░░░   ░░░░░░░░░          ░░░      ░░░░░  ░░░░░░░░  ░░░░░░   ░░░░░░  ")
-    console.print("                                                                                                      ")
-    console.print("                                                                                                      ")
-    console.print("                                                                                                      ")
-    console.print("         █████████                                                    █████                           ")
-    console.print("        ███░░░░░███                                                  ░░███                            ")
-    console.print("       ███     ░░░   ██████  ████████    ██████  ████████   ██████   ███████    ██████  ████████      ")
-    console.print("      ░███          ███░░███░░███░░███  ███░░███░░███░░███ ░░░░░███ ░░░███░    ███░░███░░███░░███     ")
-    console.print("      ░███    █████░███████  ░███ ░███ ░███████  ░███ ░░░   ███████   ░███    ░███ ░███ ░███ ░░░      ")
-    console.print("      ░░███  ░░███ ░███░░░   ░███ ░███ ░███░░░   ░███      ███░░███   ░███ ███░███ ░███ ░███          ")
-    console.print("       ░░█████████ ░░██████  ████ █████░░██████  █████    ░░████████  ░░█████ ░░██████  █████         ")
-    console.print("        ░░░░░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░      ░░░░░░░░    ░░░░░   ░░░░░░  ░░░░░          ")
-                                                                                        
+    # console.print(r".███████████   █████████   █████         █████████     █████   █████  ███      █████                 .", justify="center")
+    # console.print(r"░█░░░███░░░█  ███░░░░░███ ░░███         ███░░░░░███   ░░███   ░░███  ░░░      ░░███                  .", justify="center")
+    # console.print(r"░   ░███  ░  ░███    ░███  ░███        ███     ░░░     ░███    ░███  ████   ███████   ██████   ██████.", justify="center")
+    # console.print(r".   ░███     ░███████████  ░███       ░███             ░███    ░███ ░░███  ███░░███  ███░░███ ███░░███", justify="center")
+    # console.print(r".   ░███     ░███░░░░░███  ░███       ░███             ░░███   ███   ░███ ░███ ░███ ░███████ ░███ ░███", justify="center")
+    # console.print(r".   ░███     ░███    ░███  ░███      █░░███     ███     ░░░█████░    ░███ ░███ ░███ ░███░░░  ░███ ░███", justify="center")
+    # console.print(r".   █████    █████   █████ ███████████ ░░█████████        ░░███      █████░░████████░░██████ ░░██████.", justify="center")
+    # console.print(r".  ░░░░░    ░░░░░   ░░░░░ ░░░░░░░░░░░   ░░░░░░░░░          ░░░      ░░░░░  ░░░░░░░░  ░░░░░░   ░░░░░░ .", justify="center")
+    # console.print(r"                                                                                                      ", justify="center")
+    # console.print(r"                                                                                                      ", justify="center")
+    # console.print(r"                                                                                                      ", justify="center")
+    # console.print(r".        █████████                                                    █████                          .", justify="center")
+    # console.print(r".       ███░░░░░███                                                  ░░███                           .", justify="center")
+    # console.print(r".      ███     ░░░   ██████  ████████    ██████  ████████   ██████   ███████    ██████  ████████     .", justify="center")
+    # console.print(r".     ░███          ███░░███░░███░░███  ███░░███░░███░░███ ░░░░░███ ░░░███░    ███░░███░░███░░███    .", justify="center")
+    # console.print(r".     ░███    █████░███████  ░███ ░███ ░███████  ░███ ░░░   ███████   ░███    ░███ ░███ ░███ ░░░     .", justify="center")
+    # console.print(r".     ░░███  ░░███ ░███░░░   ░███ ░███ ░███░░░   ░███      ███░░███   ░███ ███░███ ░███ ░███         .", justify="center")
+    # console.print(r".      ░░█████████ ░░██████  ████ █████░░██████  █████    ░░████████  ░░█████ ░░██████  █████        .", justify="center")
+    # console.print(r".       ░░░░░░░░░   ░░░░░░  ░░░░ ░░░░░  ░░░░░░  ░░░░░      ░░░░░░░░    ░░░░░   ░░░░░░  ░░░░░         .", justify="center")
+    # console.print("")                                                                    
+    console.print(r". ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄       ▄               ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ .", justify="center")
+    console.print(r".▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌     ▐░▌             ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌.", justify="center")
+    console.print(r". ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀▀▀       ▐░▌           ▐░▌  ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌                 ▐░▌         ▐░▌       ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌                  ▐░▌       ▐░▌        ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░░░░░░░░░░░▌▐░▌          ▐░▌                   ▐░▌     ▐░▌         ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌                    ▐░▌   ▐░▌          ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌                     ▐░▌ ▐░▌           ▐░▌     ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄             ▐░▐░▌        ▄▄▄▄█░█▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌.", justify="center")
+    console.print(r".     ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌             ▐░▌        ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌.", justify="center")
+    console.print(r".      ▀       ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀               ▀          ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ .", justify="center")
+    console.print(r".                                                                                                                                .", justify="center")
+    console.print(r".      ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       .", justify="center")
+    console.print(r".     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌      .", justify="center")
+    console.print(r".     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌      .", justify="center")
+    console.print(r".     ▐░▌          ▐░▌          ▐░▌▐░▌    ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌      .", justify="center")
+    console.print(r".     ▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌      .", justify="center")
+    console.print(r".     ▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌      .", justify="center")
+    console.print(r".     ▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀       .", justify="center")
+    console.print(r".     ▐░▌       ▐░▌▐░▌          ▐░▌    ▐░▌▐░▌▐░▌          ▐░▌     ▐░▌  ▐░▌       ▐░▌     ▐░▌     ▐░▌       ▐░▌▐░▌     ▐░▌        .", justify="center")
+    console.print(r".     ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ ▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌       .", justify="center")
+    console.print(r".     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌      .", justify="center")
+    console.print(r".      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀       .", justify="center")
+                                                                                                                                
     today = datetime.now()
     today_formatted = today.strftime("%Y-%m-%d")
-    console.print(f" Tyler Weston 2021/2022, today: {today_formatted}, version# {__version__}")
+    console.print(f" Tyler Weston 2021/2022, today: {today_formatted}, version# {__version__}", justify="center")
     console.rule()
 
 
